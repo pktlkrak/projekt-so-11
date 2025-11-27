fe3f8e176d65b9f39b602a0938084672f707c40e
pktlkrak <pkumka66@tlkrakowpl.onmicrosoft.com>
2025-11-27 03:40:37 +0100

    commons: Add correct names to global config

--- commons/include/global_config.h
+++ commons/include/global_config.h
@@ -1,9 +1,15 @@
 #pragma once
+// K:
 #define BRIDGE_CAPACITY 5
+// N:
 #define BOAT_CAPACITY 4
+// M:
 #define BOAT_BIKE_CAPACITY 2
+// T1:
 #define BOAT_WAIT_TIME 10
+// T2:
 #define BOAT_TRIP_TIME 5
+// R:
 #define BOAT_MAX_CYCLES 500
 
 #define DEFAULT_PERMS 0600
